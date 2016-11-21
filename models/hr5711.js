c2f436a70a791360172ac777fe54e8cb2adb4d93
masonposch <masonposch@Mason-Poschs-MacBook-Pro.local>
2016-11-21 00:44:04 -0600

    set up models for each table

---
+++ models/hr5711.js
@@ -0,0 +1,33 @@
+module.exports = function (sequelize, Datatypes){
+	
+	var hr5711 = sequelize.define('hr5711', {
+		
+		id: {
+			type: Datatypes.INTEGER,
+			primaryKey: true
+		},
+
+		state: {
+			type: Datatypes.STRING,
+		},
+
+		district: {
+			type: Datatypes.STRING,
+		},
+
+		vote: {
+			type: Datatypes.STRING,
+		},
+
+		name: {
+			type: Datatypes.STRING,
+		},
+
+		party: {
+			type: Datatypes.STRING,
+		},
+	})
+
+	return hr5711;
+	
+}
---
+++ models/hr5982.js
@@ -0,0 +1,33 @@
+module.exports = function (sequelize, Datatypes){
+	
+	var hr5982 = sequelize.define('hr5982', {
+		
+		id: {
+			type: Datatypes.INTEGER,
+			primaryKey: true
+		},
+
+		state: {
+			type: Datatypes.STRING,
+		},
+
+		district: {
+			type: Datatypes.STRING,
+		},
+
+		vote: {
+			type: Datatypes.STRING,
+		},
+
+		name: {
+			type: Datatypes.STRING,
+		},
+
+		party: {
+			type: Datatypes.STRING,
+		},
+	})
+
+	return hr5982;
+	
+}
